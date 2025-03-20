d084248385039d2b98b2323a1b7f106387d5111b
eric <148893165+ericsilvaamarante@users.noreply.github.com>
2025-03-20 18:37:45 -0300

    Add files via upload

--- balatro/script.js
+++ balatro/script.js
@@ -21,10 +21,15 @@ const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade')
 botaoDeAcessibilidade.addEventListener('click', function (){
     botaoDeAcessibilidade.classList.toggle('rotacao-botao');
     opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
-   })
-   const alternaContraste = document.getElementById('alterna-contraste')
-   alternaContraste.addEventListener('click', function(){
+})
+const alternaContraste = document.getElementById('alterna-contraste')
+alternaContraste.addEventListener('click', function(){
     document.body.classList.toggle('alto-contraste')
 })
 const botaoSelecionado = botaoDeAcessibilidade.getAttribute('aria-expanded') === 'true';
 botaoDeAcessibilidade.setAttribute('aria-expanded', !botaoSelecionado)
+
+ScrollReveal().reveal('#inicio', { delay: 500 });
+ScrollReveal().reveal('#tropicalia', { delay: 500 });
+ScrollReveal().reveal('#galeria', { delay: 500 });
+ScrollReveal().reveal('#contato', { delay: 500 });
